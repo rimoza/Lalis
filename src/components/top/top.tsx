bbd757923ea731fda98d23b75c273c2b55867c32
Ismail Hussein <hussein.ismail@etteplan.com>
2022-12-06 22:25:33 +0200

    feat: add delete history item and few other changes

--- src/components/top/top.tsx
+++ src/components/top/top.tsx
@@ -3,12 +3,12 @@ import TopLeft from './topLeft';
 import { IUser, UserType } from '../../App';
 import classes from './top.module.css';
 
-const Top = ({increment, decrement, guest, home}: {increment: (a: UserType) => void, decrement: (a: UserType) => void, guest: IUser, home: IUser}) => {
+const Top = ({ increment, decrement, guest, home }: { increment: (a: UserType) => void, decrement: (a: UserType) => void, guest: IUser, home: IUser }) => {
   return (
     <div id='top' className={classes.top}>
-      <TopLeft increment={increment} decrement ={decrement} guest ={guest} />
-      <TopRight increment={increment} decrement = {decrement} home ={home} />
-     </div>
+      <TopLeft increment={increment} decrement={decrement} guest={guest} />
+      <TopRight increment={increment} decrement={decrement} home={home} />
+    </div>
   )
 }
 
--- src/components/top/topLeft.tsx
+++ src/components/top/topLeft.tsx
@@ -4,24 +4,24 @@ import { IUser, UserType } from '../../App'
 import classes from './topLeft.module.css'
 import { AiFillCaretUp, AiFillCaretDown } from 'react-icons/ai'
 
-const TopLeft = ({increment, decrement, guest}: {increment: (guest: UserType) => void, decrement: (guest: UserType) =>void, guest: IUser}) => {
+const TopLeft = ({ increment, decrement, guest }: { increment: (guest: UserType) => void, decrement: (guest: UserType) => void, guest: IUser }) => {
   return (
     <div id='topLeft' className={classes.container}>
       <span className='uppercase font-bold'>{guest.title}</span>
-        <div>
+      <div>
         <AddButton onClick={() => increment('guest')} >
           <AiFillCaretUp className={classes.icon} />
         </AddButton>
-        </div>
-        <div className={classes.value_wrapper}>
+      </div>
+      <div className={classes.value_wrapper}>
         <span className='text-xl font-bold' >{guest.value}</span>
-        </div>
-        <div>
-        <SubButton onClick={() =>decrement('guest')}>
+      </div>
+      <div>
+        <SubButton onClick={() => decrement('guest')}>
           <AiFillCaretDown className={classes.icon} />
         </SubButton>
-        </div>
       </div>
+    </div>
   )
 }
 
