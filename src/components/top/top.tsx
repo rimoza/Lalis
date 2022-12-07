6ec3040f9f4ee30963837cbf57108cf066d94fdd
Ismail Hussein <hussein.ismail@etteplan.com>
2022-12-07 21:51:57 +0200

    --no-e

--- src/components/top/top.tsx
+++ src/components/top/top.tsx
@@ -6,6 +6,12 @@ import classes from './top.module.css';
 const Top = ({ increment, decrement, guest, home }: { increment: (a: UserType) => void, decrement: (a: UserType) => void, guest: IUser, home: IUser }) => {
   return (
     <div id='top' className={classes.top}>
+      {/*
+
+        <GameScore title="guest" increment={increment} decrement={decrement}/>
+        <GameScore title="home" increment={increment} decrement={decrement}/>
+
+       */}
       <TopLeft increment={increment} decrement={decrement} guest={guest} />
       <TopRight increment={increment} decrement={decrement} home={home} />
     </div>
